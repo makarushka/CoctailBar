bc4f45aff68d331bdafe8bf7c91613b3a61d36f2
makarushka <yuri.makarow@ya.ru>
2019-01-16 01:31:17 +0300

    Edit

--- Coctail Bar/Classes/Controllers/FilterVC.swift
+++ Coctail Bar/Classes/Controllers/FilterVC.swift
@@ -9,25 +9,9 @@
 import UIKit
 
 class FilterVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func back(_ sender: UIButton) {
-        self.navigationController?.dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
